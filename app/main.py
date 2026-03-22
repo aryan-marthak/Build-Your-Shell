99f9649bce375a6720d00cb5b44c806ba6c6bc59
Aryan Marthak <aryan.marthak1@gmail.com>
2026-03-22 11:44:31 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
         elif command.startswith("echo "):
             sys.stdout.write(command[5:] + "\n")
         elif command.startswith("type "):
-            if command[5:] in ["echo", "type", "type"]:
+            if command[5:] in ["echo", "type", "exit"]:
                 sys.stdout.write(f"{command[5:]} is a shell builtin \n")
             else:
                 sys.stdout.write(f"{command[5:]} is not found \n")
